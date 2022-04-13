f69ad1c6d47e1d4aa32eafb547859b666a8c6c3f
hanafathiyah <13520047@std.stei.itb.ac.id>
2022-04-13 10:09:46 +0700

    test CI

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+"""
+    file testing (setiap modul/file ada komentar kaya gini)
+"""
 def main():
+    """
+        test prosedur (setiap fungsi/prosedur harus ada keterangan kaya gini)
+    """
     print("Test")
 
 if __name__ == "__main__":
     main()
+# harus diakhiri enter
