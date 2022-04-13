5b6d968436acd33c7e3ab2da77af6a1fd94fc11f
hanafathiyah <13520047@std.stei.itb.ac.id>
2022-04-13 17:53:58 +0700

    delete dummy.py

--- src/calendar/display.py
+++ src/calendar/display.py
@@ -16,10 +16,10 @@ def date_selected():
 calendar = Calendar(form, setmode = "day", date_pattern = 'd/m/yy')
 calendar.pack(padx=15, pady=15)
 
-open_calendar = Button(calendar, text="SELECT THE DATE", command=date_selected) 
+open_calendar = Button(calendar, text="open to do list", command=date_selected) 
 open_calendar.pack(padx=15,pady=15)
 
-form.geometry('400x400')
+form.geometry('1080x700')
 form.title("Calendar")
 form.configure(bg="white")
 form.mainloop()
