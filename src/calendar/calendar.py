54cb3f0b0ab73858baf00409b47080ad64f027a9
hanafathiyah <13520047@std.stei.itb.ac.id>
2022-04-13 11:57:05 +0700

    add calendar.py

---
+++ src/calendar/calendar.py
@@ -0,0 +1,7 @@
+"""
+    File ini adalah file calendar.py
+    File ini berfungsi untuk menampilkan kalender kepada pengguna
+"""
+
+from tkinter import * # import tkinker untuk GUI
+from tkcalendar import *
