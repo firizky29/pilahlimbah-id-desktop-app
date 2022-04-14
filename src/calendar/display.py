fc369c2d2568641d2a466cd1f755305dff5e4af2
hanafathiyah <13520047@std.stei.itb.ac.id>
2022-04-14 15:41:03 +0700

    connect with database, still messy in GUI 🙏

--- src/calendar/display.py
+++ src/calendar/display.py
@@ -5,21 +5,31 @@
 
 from tkinter import * # import tkinker untuk GUI
 from tkcalendar import *
+import mysql.connector
 
 form = Tk()
 
 def date_selected():
+    mysqldb = mysql.connector.connect(host="pilahlimbah.mariadb.database.azure.com", user="pilahlimbah@pilahlimbah", password="BDDL&g38Mv", database = "pilahlimbahid")
     date = calendar.get_date()
     display_date = Label(text=date)
     display_date.pack(padx=2,pady=2)
+    sqlcursor = mysqldb.cursor()
+    sqlcursor.execute("SELECT * FROM task")
+    for i in sqlcursor:
+        if str(i[0]) == str(date):
+            print(i[2])
+            display_task = Label(text=i[2])
+            display_task.pack(padx=2,pady=2)
 
-calendar = Calendar(form, setmode = "day", date_pattern = 'd/m/yy')
-calendar.pack(padx=15, pady=15)
+calendar = Calendar(form, setmode = "day", date_pattern = 'yyyy-mm-dd')
+calendar.pack(padx=100, pady=100)
+# calendar.configure(color = "blue")
 
 open_calendar = Button(calendar, text="open to do list", command=date_selected) 
 open_calendar.pack(padx=15,pady=15)
 
-form.geometry('1080x700')
+form.geometry('1440x1024')
 form.title("Calendar")
-form.configure(bg="white")
+form.configure(bg="cyan")
 form.mainloop()
