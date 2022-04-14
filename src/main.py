61712efbf6574e3ade2d39ef878c296213a1864a
firizky29 <13520095@std.stei.itb.ac.id>
2022-04-14 22:25:17 +0700

    Add GUI product page

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 """
     file testing (setiap modul/file ada komentar kaya gini)
 """
+import transaction.productPage as product
+
 def main():
     """
         test prosedur (setiap fungsi/prosedur harus ada keterangan kaya gini)
     """
+    product.startPage()
     print("Test")
 
 if __name__ == "__main__":
